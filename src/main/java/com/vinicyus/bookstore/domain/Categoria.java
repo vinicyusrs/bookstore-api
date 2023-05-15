707fc2b99c08bfef341385ad85ef300efb1ac8ef
vinicyusrs <vinicyus.rs@gmail.com>
2023-05-15 18:47:09 -0300

    finalizando as classes de dominio

--- src/main/java/com/vinicyus/bookstore/domain/Categoria.java
+++ src/main/java/com/vinicyus/bookstore/domain/Categoria.java
@@ -1,15 +1,32 @@
 package com.vinicyus.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Categoria {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
+
+@Entity
+public class Categoria implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String descricao;
 
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> livros = new ArrayList<>();
 
 	public Categoria() {
