de3564900b2143865a4ad22f55c5e4df63faeb11
vinicyusrs <vinicyus.rs@gmail.com>
2023-05-15 16:16:31 -0300

    commit inicial

---
+++ src/test/java/com/vinicyus/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicyus.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
