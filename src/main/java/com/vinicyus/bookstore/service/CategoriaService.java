ca4aa6cb7821d578e4c1d4c0e02383c8f1a31b2b
vinicyusrs <vinicyus.rs@gmail.com>
2023-05-16 14:45:51 -0300

    POST para Categoria

--- src/main/java/com/vinicyus/bookstore/service/CategoriaService.java
+++ src/main/java/com/vinicyus/bookstore/service/CategoriaService.java
@@ -25,8 +25,14 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {
 		return repository.findAll();
-		
+			
+	}
+	
+	public Categoria create(Categoria obj) {
+		obj.setId(null);
+		return repository.save(obj);
 		
 	}
+	
 
 }
