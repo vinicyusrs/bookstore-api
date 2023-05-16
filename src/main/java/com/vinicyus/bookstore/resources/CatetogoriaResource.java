854dc61f81c8f843c5991df4ada63743a2a3af43
vinicyusrs <vinicyus.rs@gmail.com>
2023-05-16 17:35:11 -0300

    Update para Categorias

--- src/main/java/com/vinicyus/bookstore/resources/CatetogoriaResource.java
+++ src/main/java/com/vinicyus/bookstore/resources/CatetogoriaResource.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,17 +22,18 @@ import com.vinicyus.bookstore.service.CategoriaService;
 @RestController
 @RequestMapping(value = "/categorias")
 public class CatetogoriaResource {
-	
+
 	@Autowired
 	private CategoriaService service;
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
 		Categoria obj = service.findById(id);
-		return	ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(obj);
 	}
+
 	@GetMapping
-	public ResponseEntity<List<CategoriaDTO>> findAll(){
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
@@ -42,12 +44,17 @@ public class CatetogoriaResource {
 		obj = service.create(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoriaDTO> update(@PathVariable Integer id, @RequestBody CategoriaDTO objDto) {
+		Categoria newObj = service.update(id, objDto);
+		return ResponseEntity.ok().body(new CategoriaDTO(newObj));
+		
 		
 		
 	}
-		
+	
 }
-
-
 
 // localhost:9090/categorias/1
--- src/main/java/com/vinicyus/bookstore/service/CategoriaService.java
+++ src/main/java/com/vinicyus/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vinicyus.bookstore.domain.Categoria;
+import com.vinicyus.bookstore.dtos.CategoriaDTO;
 import com.vinicyus.bookstore.repositories.CategoriaRepository;
 import com.vinicyus.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -32,6 +33,13 @@ public class CategoriaService {
 		obj.setId(null);
 		return repository.save(obj);
 		
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
+		return repository.save(obj);
 	}
 	
 
