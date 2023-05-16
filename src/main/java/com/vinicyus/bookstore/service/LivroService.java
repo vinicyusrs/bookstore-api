92b407e9794544f6c3f6aef5bb984aa78f1626d2
vinicyusrs <vinicyus.rs@gmail.com>
2023-05-16 20:18:31 -0300

    Post para cadastrar livro por categoria

--- src/main/java/com/vinicyus/bookstore/service/LivroService.java
+++ src/main/java/com/vinicyus/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.vinicyus.bookstore.domain.Categoria;
 import com.vinicyus.bookstore.domain.Livro;
 import com.vinicyus.bookstore.repositories.LivroRepository;
 import com.vinicyus.bookstore.service.exceptions.ObjectNotFoundException;
@@ -40,6 +41,14 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
+		
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 		
 	}
 }
