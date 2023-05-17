a3da82eb71669e9aacb1775f2fb2b77b3f15296e
vinicyusrs <vinicyus.rs@gmail.com>
2023-05-17 15:33:35 -0300

    mais uma tentativa retirando @repository

--- src/main/java/com/vinicyus/bookstore/repositories/LivroRepository.java
+++ src/main/java/com/vinicyus/bookstore/repositories/LivroRepository.java
@@ -14,7 +14,4 @@ public interface LivroRepository extends JpaRepository<Livro, Integer> {
 
 	@Query("SELECT obj FROM Livro obj Where obj.categoria.id = :id_cat ORDER BY titulo")
 	List<Livro> findAllByCategoria(@Param(value = "id_cat")Integer id_cat);
-
-	
-
 }
